59594b348cecc871908a618137beed8f809391ae
Parshva Runwal <runwalparshva3@gmail.com>
2024-02-04 02:02:29 +0530

    bootstrapping client again

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,65 +1,6 @@
-"use client";
-
 import Image from "next/image";
-import { useEffect } from "react";
-
-import { ZkBobClient, ClientConfig, AccountConfig, ProverMode, TransferRequest, deriveSpendingKeyZkBob, DepositType, TxType } from "zkbob-client-js";
-
-async function zkBobExample(): Promise<void> {
-  // Client configuration includes set of pools, chains, parameters and other options
-  const clientConfig: ClientConfig = {
-    pools: {
-      "BOB2USDC-goerli": {
-        chainId: 5,
-        poolAddress: "0x49661694a71B3Dab9F25E86D5df2809B170c56E6",
-        tokenAddress: "0x28B531401Ee3f17521B3772c13EAF3f86C2Fe780",
-        relayerUrls: ["https://dev-relayer.thgkjlr.website/"],
-        delegatedProverUrls: [],
-        feeDecimals: 2,
-        depositScheme: DepositType.AuthPolygonUSDC,
-        ddSubgraph: "zkbob-bob-goerli",
-      },
-    },
-  };
-
-  // creating a zkBob client without account to be worked on 'BOB-sepolia' pool
-  const client = await ZkBobClient.create(clientConfig, "BOB2USDC-goerli");
-
-  // now you can get relayer fee or pool limits for example
-  // const depositFee = await client.atomicTxFee(TxType.BridgeDeposit);
-  // console.log(`Relayer deposit fee: ${depositFee} Gwei`);
-  // console.log(`Pool deposit total limit: ${(await client.getLimits(undefined)).deposit.total} Gwei`);
-
-  // now let's attach account generated from the arbitrary 12-words mnemonic:
-  const mnemonic = "magic jargon demon hello marriage happy bench wash doctor risk end cheap";
-  const accountConfig: AccountConfig = {
-    // spending key is a byte array which derived from mnemonic
-    sk: deriveSpendingKeyZkBob(mnemonic),
-    // pool alias which should be activated
-    pool: "BOB2USDC-goerli",
-    // the account should have no activity (incoming notes including) before that index
-    // you can use -1 value only for newly created account or undefined (or 0) for full state sync
-    birthindex: 0,
-    // using local prover
-    proverMode: ProverMode.Local,
-  };
-  await client.login(accountConfig);
-
-  // now the client is ready to send transactions, but let's get an account balance first
-  // account state will be synced under-the-hood (pass false to getTotalBalance to prevent sync)
-
-  console.log(`HAS ACCOUNT: ${client.hasAccount()}`);
-
-  console.log(`ACCOUNT BALANCE: ${await client.getTotalBalance()}`);
-
-  await client.logout();
-}
 
 export default function Home() {
-  useEffect(() => {
-    zkBobExample();
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -74,13 +15,28 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={100} height={24} priority />
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
           </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -91,9 +47,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Find in-depth information about Next.js features and API.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
         </a>
 
         <a
@@ -103,9 +64,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
         </a>
 
         <a
@@ -115,9 +81,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Explore starter templates for Next.js.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+          </p>
         </a>
 
         <a
@@ -127,9 +98,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
         </a>
       </div>
     </main>
